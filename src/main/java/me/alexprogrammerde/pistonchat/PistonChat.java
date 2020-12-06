a9874d7c013bc3946b4e80e9a9367292805554e1
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-12-06 10:49:05 +0100

    Init commit

---
+++ src/main/java/me/alexprogrammerde/pistonchat/PistonChat.java
@@ -0,0 +1,16 @@
+package me.alexprogrammerde.pistonchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PistonChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
