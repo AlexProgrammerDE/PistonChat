2a5901195cd8b1ef3d9692bde3a74dccb69ba3be
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-04-01 16:42:42 +0200

    Fix error

--- src/main/java/net/pistonmaster/pistonchat/events/ChatEvent.java
+++ src/main/java/net/pistonmaster/pistonchat/events/ChatEvent.java
@@ -30,10 +30,6 @@ public class ChatEvent implements Listener {
 
         event.setCancelled(pistonChatEvent.isCancelled());
 
-        if (PlatformUtils.isMultiPaper()) {
-            event.setCancelled(true);
-        }
-
         if (!pistonChatEvent.isCancelled()) {
             String message = pistonChatEvent.getMessage();
 
--- src/main/java/net/pistonmaster/pistonchat/utils/PlatformUtils.java
+++ src/main/java/net/pistonmaster/pistonchat/utils/PlatformUtils.java
@@ -1,11 +1,9 @@
 package net.pistonmaster.pistonchat.utils;
 
 import com.github.puregero.multilib.MultiLib;
-import com.github.puregero.multilib.multipaper.MultiPaperImpl;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.UUID;
 
@@ -20,14 +18,5 @@ public class PlatformUtils {
 
     public static Collection<? extends Player> getOnlinePlayers() {
         return MultiLib.getAllOnlinePlayers();
-    }
-
-    public static boolean isMultiPaper() {
-        try {
-            return MultiLib.class.getDeclaredMethod("get").invoke(null) instanceof MultiPaperImpl;
-        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
-            return false;
-        }
     }
 }
